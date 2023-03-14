3bf95acb86e086730866d4351245ad9d066b3780
Camilo Fontecha <cafontecha@hotmail.com>
2023-03-14 01:41:27 -0500

    Entendiendo clases

---
+++ Clase 1/index.js
@@ -0,0 +1,25 @@
+class Cliente{
+    nombreCliente;
+    iDCliente;
+    numCuenta;
+    saldoCuenta;
+}
+
+const cliente1 = new Cliente();
+cliente1.nombreCliente = 'Camilo';
+cliente1.iDCliente = '123456';
+cliente1.numCuenta = '12346';
+cliente1.saldoCuenta = 1000;
+
+const cliente2 = new Cliente();
+cliente2.nombreCliente = 'Andres';
+cliente2.iDCliente = '567890';
+cliente2.numCuenta = '567890';
+cliente2.saldoCuenta = 8000;
+
+const cliente3 = new Cliente();
+cliente3.nombreCliente = 'Juan';
+cliente3.iDCliente = '987654';
+cliente3.numCuenta = '987654';
+cliente3.saldoCuenta = 5600;
+
