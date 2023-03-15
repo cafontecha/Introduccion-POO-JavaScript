01d4fc1c8b57a54fe9c29fe17b4ed233041dbe83
Camilo Fontecha <cafontecha@hotmail.com>
2023-03-14 22:03:16 -0500

    Añadiendo variables privadas y optimizando depositoSaldo

--- Clase 1/index.js
+++ Clase 1/index.js
@@ -4,26 +4,31 @@ class Cliente{
 }
 class cuentaCorriente {
     numCuenta;
-    saldoCuenta;
+    #saldoCuenta;
     sucursal;
     
     constructor(){
         this.numCuenta = '';
-        this.saldoCuenta = 0;
+        this.#saldoCuenta = 0;
         this.sucursal = '';
     }
 
     depositoCuenta(valor){
-        this.saldoCuenta+=valor;
+        if (valor > 0 ){
+            this.#saldoCuenta+=valor;
+            console.log(`Tu nuevo saldo es ${this.#saldoCuenta}`);
+        }
+        else{
+            console.log(`debes depositar una cantidad mayor a ${valor}`);
+        }
     }
 
     retiroCuenta(valor){
-        const nuevoSaldo = this.saldoCuenta - valor;
-        if (nuevoSaldo<=0){
-            console.log(`No cuentas con el saldo a retirar, tu saldo es de ${this.saldoCuenta}`);
+        if (this.#saldoCuenta<=valor){
+            console.log(`No cuentas con el saldo a retirar, tu saldo es de ${this.#saldoCuenta}`);
         }else{
-            console.log(`retiro exitoso, tu nuevo saldo es de ${nuevoSaldo}`);
-            this.saldoCuenta = nuevoSaldo;
+            this.#saldoCuenta -= valor;
+            console.log(`retiro exitoso, tu nuevo saldo es de ${this.#saldoCuenta}`);
         } 
     }
 }
@@ -35,30 +40,10 @@ cliente1.iDCliente = '123456';
 
 const cuentaCorriente1 = new cuentaCorriente(); 
 cuentaCorriente1.numCuenta = '12346';
-cuentaCorriente1.saldoCuenta = 1000;
+//Al poner el atributo privado, no se pueden hacer operaciones 
+//fuera del instanciamiento de la clase y metodos
+//cuentaCorriente1.#saldoCuenta = 1000;
 cuentaCorriente1.sucursal = "Bogota";
-
-const cliente2 = new Cliente();
-cliente2.nombreCliente = 'Andres';
-cliente2.iDCliente = '567890';
-
-const cuentaCorriente2 = new cuentaCorriente();
-cuentaCorriente2.numCuenta = '567890';
-cuentaCorriente2.saldoCuenta = 8000;
-cuentaCorriente2.sucursal = "Centro Comercial";
-
-const cliente3 = new Cliente();
-cliente3.nombreCliente = 'Juan';
-cliente3.iDCliente = '987654';
-
-const cuentaCorriente3 = new cuentaCorriente();
-cuentaCorriente3.numCuenta = '987654';
-cuentaCorriente3.saldoCuenta = 5600;
-cuentaCorriente3. sucursal = "Plaza Central";
-
-
-console.log(cliente1);
-console.log(cuentaCorriente1);
 
 
 console.log(cuentaCorriente1);
